9f4146c14cb12f3a3888c7c78d7da651139d3812
Sammy <sammyoziegbe@gmail.com>
2022-06-16 16:17:34 +0100

    code cleanup

--- server.js
+++ server.js
@@ -1,49 +1,15 @@
 const app = require('./app')
 const http = require('http')
-const socketIo = require("socket.io");
+const socketIo = require("./socketIo");
 const { mongoConnect } = require('./services/mongo.service');
 
 const PORT = process.env.PORT || 5000
-const NODE_ENV = process.env.NODE_ENV
-
 
 mongoConnect()
 
 const server = http.createServer(app)
 
-const io = socketIo(server, {
-    cors: {
-      origin: NODE_ENV === 'production' 
-        ? "https://samuel-travel-memories.netlify.app"
-        : "localhost:3000",
-      methods: ["GET", "POST"],
-      credentials: true
-    }
-});
-
-// const getApiAndEmit = socket => {
-//     const response = new Date();
-//     socket.emit("FromAPI", response);
-// };
-  
-
-io.on('connection', (socket) => {
-    console.log('client connected!')
-    // getApiAndEmit(socket)
-
-    socket.on('setup', ({ room }) => {
-        socket.join(room)
-    })
-
-    socket.on('new_message', ({ room, messageData }) => {
-        io.in(room).emit('message', messageData)
-        console.log(room, messageData)
-    })
-
-    socket.on("disconnect", () => {
-        console.log("Client disconnected");
-    });
-})
+socketIo(server)
 
 server.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
---
+++ socketIo.js
@@ -0,0 +1,43 @@
+const socketIo = require("socket.io");
+
+const NODE_ENV = process.env.NODE_ENV
+
+function liveSocket(server) {
+
+    const io = socketIo(server, {
+        cors: {
+            origin: NODE_ENV === 'production' 
+                ? "https://samuel-travel-memories.netlify.app"
+                : "localhost:3000",
+            methods: ["GET", "POST"],
+            credentials: true
+        }
+    });
+
+    // const getApiAndEmit = socket => {
+    //     const response = new Date();
+    //     socket.emit("FromAPI", response);
+    // };
+    
+    io.on('connection', (socket) => {
+        console.log('client connected!')
+        // getApiAndEmit(socket)
+
+        socket.on('setup', ({ room }) => {
+            socket.join(room)
+        })
+
+        socket.on('new_message', ({ room, messageData }) => {
+            io.in(room).emit('message', messageData)
+            console.log(room, messageData)
+        })
+
+        socket.on("disconnect", () => {
+            console.log("Client disconnected");
+        });
+    })
+
+    return io
+}
+
+module.exports = liveSocket
